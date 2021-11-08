4a1d85c96f49324b24ca20f013682eac4531892c
rh-huang <576788582@qq.com>
2021-11-08 18:40:08 +0800

    修改编译错误

--- src/observer/sql/parser/parse_defs.h
+++ src/observer/sql/parser/parse_defs.h
@@ -23,8 +23,8 @@ See the Mulan PSL v2 for more details. */
 #define MAX_ATTR_NAME 20
 #define MAX_ERROR_MESSAGE 20
 #define MAX_DATA 50
-#define FLT_MIN 1.17549435e-38F
-#define INT_MAX -2147483648
+#define OB_FLT_MIN 1.17549435e-38F
+#define OB_INT_MIN -2147483648
 
 //属性结构体
 typedef struct {
