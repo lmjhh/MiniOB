e916e285afe3d484e362e6d2f25af58c1f028901
rh-huang <576788582@qq.com>
2021-11-14 17:45:25 +0800

    终版

--- src/observer/sql/executor/execute_stage.cpp
+++ src/observer/sql/executor/execute_stage.cpp
@@ -40,7 +40,7 @@ RC table_Join_execute(TupleSet &table1, TupleSet &table2, const Selects &selects
 bool filter_tuple(const std::shared_ptr<TupleValue> &values1, const std::shared_ptr<TupleValue> &values2, CompOp op);
 TupleSet get_final_result(const Selects &selects, TupleSet &full_tupleSet);
 RC get_ploy_tupleSet(const Poly poly_list[], int poly_num, TupleSet &full_tupleSet, TupleSet &resultTupleSet);
-TupleSet group_by_field(const Selects &selects, TupleSet &full_tupleSet, bool isMultiTable);
+RC group_by_field(const Selects &selects, TupleSet &full_tupleSet, TupleSet &resultTupleSet);
 //! Constructor
 ExecuteStage::ExecuteStage(const char *tag) : Stage(tag) {}
 
@@ -289,9 +289,8 @@ RC ExecuteStage::do_select(const char *db, Query *sql, SessionEvent *session_eve
     }
     rc = join_result_tupleSet.order_by_field_and_type(selects.order_by.attributes, selects.order_by.order_type, selects.order_by.attr_num);
     if(selects.poly_num > 0 && selects.attr_num > 0 && selects.group_by.attr_num == selects.attr_num){
-      result_tupleSet = group_by_field(selects, join_result_tupleSet, true);
-      // return RC::SUCCESS;
-      // if(rc != RC::SUCCESS) return rc;
+      rc = group_by_field(selects, join_result_tupleSet, result_tupleSet);
+      if(rc != RC::SUCCESS) return rc; 
     }else{
       result_tupleSet = get_final_result(selects, join_result_tupleSet);
     }
@@ -304,9 +303,8 @@ RC ExecuteStage::do_select(const char *db, Query *sql, SessionEvent *session_eve
         rc = get_ploy_tupleSet(selects.poly_list, selects.poly_num, tuple_sets.front(), result_tupleSet);
       }else if(selects.poly_num > 0 && selects.attr_num > 0 && selects.group_by.attr_num == selects.attr_num){
         LOG_ERROR("单表做Group by");
-        result_tupleSet = group_by_field(selects, tuple_sets.front(), false);
-        // return RC::SUCCESS;
-        // if(rc != RC::SUCCESS) return rc;          
+        rc = group_by_field(selects, tuple_sets.front(), result_tupleSet);
+        if(rc != RC::SUCCESS) return rc;          
       } else {
         rc = tuple_sets.front().order_by_field_and_type(selects.order_by.attributes, selects.order_by.order_type, selects.order_by.attr_num);
         result_tupleSet = get_final_result(selects, tuple_sets.front());
@@ -801,12 +799,11 @@ RC get_ploy_tupleSet(const Poly poly_list[], int poly_num, TupleSet &full_tupleS
   return RC::SUCCESS;
 }
 
-TupleSet group_by_field(const Selects &selects, TupleSet &full_tupleSet, bool isMultiTable){
-  TupleSet resultTupleSet;
+RC group_by_field(const Selects &selects, TupleSet &full_tupleSet, TupleSet &resultTupleSet){
   RC rc;
   rc = full_tupleSet.order_by_field_and_type(selects.group_by.attributes, selects.group_by.order_type, selects.group_by.attr_num);   
   if(rc != RC::SUCCESS){
-    return resultTupleSet;
+    return rc;
   }
 
   std::stringstream bb;
@@ -855,7 +852,7 @@ TupleSet group_by_field(const Selects &selects, TupleSet &full_tupleSet, bool is
 
     TupleSet sub_tupleSet;
     rc = get_ploy_tupleSet(selects.poly_list, selects.poly_num, new_tupleSet, sub_tupleSet);
-    if(rc != SUCCESS) return resultTupleSet;
+    if(rc != SUCCESS) return rc;
 
     std::stringstream ss;
     sub_tupleSet.print(ss, false);
@@ -868,16 +865,17 @@ TupleSet group_by_field(const Selects &selects, TupleSet &full_tupleSet, bool is
     for (int index = selects.lsn - 1; index >= 0; index--){
       int value1_index = -1;
       int value2_index = -1;
+      const TupleField *tmpField = nullptr;
       for(int attrIndex = selects.attr_num - 1; attrIndex >= 0 ; attrIndex--){
         if(selects.attributes[attrIndex].lsn == index){
           if(selects.attributes[attrIndex].relation_name != nullptr){
             value1_index = full_tupleSet.get_schema().index_of_field(selects.attributes[attrIndex].relation_name, selects.attributes[attrIndex].attribute_name);
-            const TupleField *tmpField = &full_tupleSet.get_schema().field(value1_index);
+            tmpField = &full_tupleSet.get_schema().field(value1_index);
             schema.add_if_not_exists(tmpField->type(), tmpField->table_name(), tmpField->field_name());
           }else{
             const char *table_name = full_tupleSet.get_schema().field(0).table_name();
             value1_index = full_tupleSet.get_schema().index_of_field(table_name, selects.attributes[attrIndex].attribute_name);
-            const TupleField *tmpField = &full_tupleSet.get_schema().field(value1_index);  
+            tmpField = &full_tupleSet.get_schema().field(value1_index);  
             schema.add_if_not_exists(tmpField->type(), tmpField->table_name(), tmpField->field_name());        
           }
           break;
@@ -886,9 +884,8 @@ TupleSet group_by_field(const Selects &selects, TupleSet &full_tupleSet, bool is
       for(int polyIndex = 0; polyIndex < selects.poly_num ; polyIndex++){
         if(selects.poly_list[polyIndex].lsn == index){
           TupleSchema tmpSchema = sub_tupleSet.get_schema();
-          const TupleField *tmpField = &tmpSchema.field(selects.poly_num - polyIndex - 1);
+          tmpField = &tmpSchema.field(selects.poly_num - polyIndex - 1);
           value2_index = selects.poly_num - polyIndex - 1;
-          schema.add_if_not_exists(tmpField->type(), tmpField->table_name(), tmpField->field_name());
           break;
         }
       }
@@ -900,6 +897,7 @@ TupleSet group_by_field(const Selects &selects, TupleSet &full_tupleSet, bool is
         LOG_ERROR("add value %f ",sub_values[value2_index]->getValue());
         result_tuple.add(sub_values[value2_index]);
       }
+      schema.add_if_not_exists(tmpField->type(), tmpField->table_name(), tmpField->field_name());
     }
     resultTupleSet.add(std::move(result_tuple));
   }
@@ -910,5 +908,5 @@ TupleSet group_by_field(const Selects &selects, TupleSet &full_tupleSet, bool is
   resultTupleSet.print(cc, false);
   std::cout << cc.str() << std::endl;
 
-  return resultTupleSet;
+  return RC::SUCCESS;
 }
