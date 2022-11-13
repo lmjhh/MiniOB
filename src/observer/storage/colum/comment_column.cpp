485976c3eaf45937d1c146d13437fcbe84b59e97
RH Huang <576788582@qq.com>
2022-11-13 11:15:51 +0800

    comment 使用bzip3
    111351527 -> 99517468

--- src/observer/storage/colum/comment_column.cpp
+++ src/observer/storage/colum/comment_column.cpp
@@ -77,26 +77,26 @@ void CommentColumn::create_file(std::string file_name) {
 }
 
 void CommentColumn::open_file(std::string file_name) {
+  file_name_ = file_name;
+//  for(int i = 35; i < u8_to_str.size(); i++) {
+//    if (words_map[u8_to_str[i]] != i) {
+//      printf ("%d\n",i);
+//    }
+//  }
 
-//  bzip3_uncompress_file(file_name.c_str(), CommentColumnValueCache);
+//  lzw_uncompress_file(file_name.c_str());
+//  std::string remove_file = file_name + ".lzw";
+//  remove(remove_file.c_str());
+//
+//  std::ifstream in(file_name.c_str(), std::ios::in);
+//  in.read((char *) (&current_offset_), 4);
+//  in.read((char *) CommentColumnValueCache + 4 , current_offset_);
+//  in.close();
+}
 
-  for(int i = 35; i < u8_to_str.size(); i++) {
-    if (words_map[u8_to_str[i]] != i) {
-      printf ("%d\n",i);
-    }
-  }
-
-  lzw_uncompress_file(file_name.c_str());
-  std::string remove_file = file_name + ".lzw";
-  remove(remove_file.c_str());
-
-  std::ifstream in(file_name.c_str(), std::ios::in);
-  in.read((char *) (&current_offset_), 4);
-  in.read((char *) CommentColumnValueCache + 4 , current_offset_);
-  in.close();
-
-//  current_offset_ = *(uint32_t *)CommentColumnValueCache;
-
+void CommentColumn::delay_open_file() {
+  bzip3_uncompress_file(file_name_.c_str(), CommentColumnValueCache);
+  current_offset_ = *(uint32_t *)CommentColumnValueCache;
   for (int i = 4; i < current_offset_ + 4; i++) {
     if (CommentColumnValueCache[i] >= 128) {
       CommentColumnValueCache[i] -= 128; //还原字符
@@ -106,6 +106,9 @@ void CommentColumn::open_file(std::string file_name) {
 }
 
 void CommentColumn::to_string(std::ostream &os, int index, int line_num) {
+  if (total_data_ == 0) {
+    delay_open_file();
+  }
   std::string result;
   int begin_index = 4;
   if (line_num > 0) begin_index = CommentColumnValueOffset[line_num - 1];
@@ -140,12 +143,12 @@ void CommentColumn::insert(void *data, int index) {
 void CommentColumn::flush_to_disk() {
   memcpy(CommentColumnValueCache, &current_offset_, 4);
 
-//  bzip3_compress_file(file_name_, CommentColumnValueCache, current_offset_);
+  bzip3_compress_file(file_name_, CommentColumnValueCache, current_offset_);
 
-  std::ofstream out(file_name_.c_str(), std::ios::out);
-  out.write((const char *) CommentColumnValueCache, current_offset_);
-  out.close();
-
-  lzw_compress_file(file_name_);
-  remove(file_name_.c_str());
+//  std::ofstream out(file_name_.c_str(), std::ios::out);
+//  out.write((const char *) CommentColumnValueCache, current_offset_);
+//  out.close();
+//
+//  lzw_compress_file(file_name_);
+//  remove(file_name_.c_str());
 }
--- src/observer/storage/colum/comment_column.h
+++ src/observer/storage/colum/comment_column.h
@@ -13,6 +13,8 @@ public:
   void to_string(std::ostream &os, int index, int line_num) override;
   void insert(void *data, int index) override;
   void flush_to_disk() override;
+
+  void delay_open_file();
 private:
   uint32_t current_offset_ = 4;
   uint32_t total_data_ = 0;
